71fe9857a952ac1d020a30439c8af1d2ba23b407
Gabriel Marinho <jgabriel.marinho.dev@hotmail.com>
2023-09-12 16:38:45 -0300

    style: finished header style

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,26 @@
+import { Github } from 'lucide-react'
 import { Button } from './components/ui/button'
+import { Separator } from './components/ui/separator'
 
 export function App() {
-  return <Button>Teste</Button>
+  return (
+    <div>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl uppercase font-bold">Upload.IA</h1>
+
+        <div className="flex items-center gap-3">
+          <span className="text-sm text-muted-foreground">
+            Desenvolvido com ❤ no NLW da Rocketseat
+          </span>
+
+          <Separator orientation="vertical" className="h-6" />
+
+          <Button variant={'outline'}>
+            <Github className="w-4 h-4 mr-2" />
+            Github
+          </Button>
+        </div>
+      </div>
+    </div>
+  )
 }
